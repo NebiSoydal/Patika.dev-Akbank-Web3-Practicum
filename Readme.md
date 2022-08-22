10fa7ff06199eb48d3a38cb08c0e4e6fcf172813
Nebı Soydal <woozdersoftware@gmail.com>
2022-08-22 15:11:19 +0300

    Patika Akbank Web3 Case App ve Readme Dosyası

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Patika.dev Akbank Web3 Practicum
+
+### Akbank Web3 Practicum için açılmış bir repodur case çözümünü içerir. </br></br>
+
+### Case Açıklaması  Ve İşleyiş Biçimi:
+* Tek Ve Çift Sayının İşlevini Belirten Basit Javascript Projesidir. Detaylar Aşağıda Mevcuttur.
+* 7 çıktı ile kontrol edilmiştir.
+* Bir sayının tüm basamakları toplamı çift ise çift tek ise tektir.
+* Sayı tek ise Odd Aksi Takdirde Even değeri döner.
+
+ ## Nebi Soydal 
+* Discord Id = "Woozder#9804"
+
---
+++ app.js
@@ -0,0 +1,28 @@
+const oddishOrEvenish = (number) => {
+    let tmp = 0;
+    const GetSeperetedArr = DoSeparatedNumber(number);
+    GetSeperetedArr.forEach((item) => {
+      tmp += +item;
+    });
+    if (tmp % 2 === 1) {
+      return 'Odd';
+    }
+    return 'Even';
+  };
+  
+  const DoSeparatedNumber = (number) => {
+    let separatedArr = [];
+    const separatedNumber = number.toString().split('');
+    separatedNumber.forEach((item) => {
+      separatedArr.push(item);
+    });
+    return separatedArr;
+  };
+  
+console.log(oddishOrEvenish(0));
+console.log(oddishOrEvenish(456));
+console.log(oddishOrEvenish(353));
+console.log(oddishOrEvenish(3711));
+console.log(oddishOrEvenish(43));
+console.log(oddishOrEvenish(373));
+console.log(oddishOrEvenish(4433));
